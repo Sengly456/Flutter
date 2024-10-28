8a628519783825eefd52cb4ace48d8dbc8f19991
Sengly456 <senglyou84@gmail.com>
2024-10-28 11:21:36 +0700

    committed

--- lib/W3-S2-PRACTICE/EX-4/main.dart
+++ lib/W3-S2-PRACTICE/EX-4/main.dart
@@ -1,18 +1,45 @@
-// Start from the exercice 3 code
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(CustomCard());
-}
-class CustomCard extends StatelessWidget{
-
-  final String Text;
-  final Color color;
-  const CustomCard(Text,)
+class GradientButton extends StatelessWidget {
+  final String text; 
+  final Color start;
+  final Color end;
+  const GradientButton({super.key, required this.text, required this.start,required this.end,});
   @override
-  widget build(BuildContext context){
-    return MaterialApp(
+  Widget build(BuildContext context) {
 
-    )
+    return Container(
+      padding: const EdgeInsets.all(30), 
+      margin: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft, 
+          end: Alignment.bottomRight, 
+          colors: [start, end],
+        ),
+        borderRadius: BorderRadius.circular(10), 
+      ),
+      child: Text(
+        text,
+        style: const TextStyle(color: Colors.white, fontSize: 24), 
+      ),
+    );
   }
+}
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child:  Column(
+            children: [
+            GradientButton(text: "Hello 1", start: Colors.red, end: Colors.green),
+            GradientButton(text: "Hello 2", start: Colors.green, end: Colors.blue),
+            GradientButton(text: "Hello 3", start: Colors.blue, end: Colors.green),
+          ]),
+        ),
+      ),
+    ),
+  );
 }
